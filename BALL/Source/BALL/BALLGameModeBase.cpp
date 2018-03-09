3f327a045c5b4be50cfeec667d883b41376f3053
levani-PC\levani <leonsiusgit@gmail.com>
2018-03-09 13:23:30 +0400

    BG03 BALL Unreal Project Files

---
+++ BALL/Source/BALL/BALLGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BALL.h"
+#include "BALLGameModeBase.h"
+
+
+
+
---
+++ BALL/Source/BALL/BALLGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "BALLGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BALL_API ABALLGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
