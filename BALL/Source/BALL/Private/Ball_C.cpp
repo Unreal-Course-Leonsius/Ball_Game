c3d30e228de7334a10248fc55c55720b0b27a498
levani-PC\levani <leonsiusgit@gmail.com>
2018-03-18 01:19:16 +0400

    BG07 Ball Movement

--- BALL/Source/BALL/Private/Ball_C.cpp
+++ BALL/Source/BALL/Private/Ball_C.cpp
@@ -10,13 +10,35 @@ ABall_C::ABall_C()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	RollTorque = 500000000.0f;
+	//JumpImpulse = 350000.0f;
+	bCanJump = true; // Start being able to jump
+}
+
+void ABall_C::Initialize(UStaticMeshComponent * ball, USceneComponent * scene, USpringArmComponent * springarm, UCameraComponent* camera)
+{
+	/* Get Components */
+	Ball = ball;
+	Scene = scene;
+	SpringArm = springarm;
+	Camera = camera;
+
+	Setting();
+}
+
+void ABall_C::Setting()
+{
+	Scene->bAbsoluteRotation = true; // Rotation of the ball should not affect rotation of boom
+	SpringArm->bDoCollisionTest = false;
+	//SpringArm->bAbsoluteRotation = true; // must not be bAbsolutRotation = true because Scene Component work
 }
 
 // Called when the game starts or when spawned
 void ABall_C::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+
 }
 
 // Called every frame
@@ -31,5 +53,51 @@ void ABall_C::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	// Mouse Movement
+	PlayerInputComponent->BindAxis("Athimuth", this, &ABall_C::Azimuth);
+	PlayerInputComponent->BindAxis("Elevation", this, &ABall_C::Elevation);
+
+	// Forwad Backward
+	PlayerInputComponent->BindAxis("WForward_Backward", this, &ABall_C::MoveForward);
+	PlayerInputComponent->BindAxis("ALeft_DRight", this, &ABall_C::MoveRight);
+
 }
 
+
+void ABall_C::Azimuth(float Val)
+{
+	//auto NewRotation = FRotator(0.f, 0.f, Val * GetWorld()->GetDeltaSeconds() * 50);
+	//Scene->SetRelativeRotation(NewRotation);
+
+	auto pitch = Val * GetWorld()->GetDeltaSeconds() * 50;
+	auto NewRotation = FRotator(0.f, pitch, 0.f); 
+	Scene->AddLocalRotation(NewRotation);
+
+
+}
+
+void ABall_C::Elevation(float Val)
+{
+	auto NewRotation = FRotator(Val * GetWorld()->GetDeltaSeconds() * 50, 0.f, 0.f);
+	SpringArm->AddLocalRotation(NewRotation);
+}
+
+void ABall_C::MoveRight(float Val)
+{
+	const FVector Torque = FVector(-1.f * Val * RollTorque, 0.f, 0.f);
+	//Ball->AddTorque(Torque);
+	//auto ForceApply = Ball->GetRightVector() * Val * 5000000; // We need not GetRightVector() because when Ball collide something right and forward vector is messed
+	auto WorldDirection = GetActorLocation();
+	WorldDirection += FVector(0.f, Val * GetWorld()->GetDeltaSeconds() * 1000, 0.f);
+
+	SetActorLocation(WorldDirection);
+}
+
+void ABall_C::MoveForward(float Val)
+{
+	const FVector Torque = FVector(0.f, Val * RollTorque, 0.f);
+	Ball->AddTorque(Torque);	// Ball->AddTorque(Torque, FName(TEXT("None")), true); true means that torque apply no attantion Ball mass it rotate itself
+	auto WorldDirection = GetActorLocation();
+	WorldDirection += FVector(Val * GetWorld()->GetDeltaSeconds() * 2000, 0.f, 0.f);
+	SetActorLocation(WorldDirection);
+}
--- BALL/Source/BALL/Public/Ball_C.h
+++ BALL/Source/BALL/Public/Ball_C.h
@@ -10,13 +10,45 @@ class BALL_API ABall_C : public APawn
 {
 	GENERATED_BODY()
 
+	/** StaticMesh used for the ball */
+	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Ball, meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent *Ball;
+
+	class USceneComponent *Scene;
+
+	class USpringArmComponent *SpringArm;
+
+	class UCameraComponent* Camera;
+
 public:
 	// Sets default values for this pawn's properties
 	ABall_C();
 
+	/** Torque to apply when trying to roll ball */
+	UPROPERTY(EditAnywhere, Category = "Ball")
+	float RollTorque;
+
+	/** Indicates whether we can currently jump, use to prevent double jumping */
+	bool bCanJump;
+
+	UFUNCTION(BlueprintCallable, Category = "Setup")
+	void Initialize(UStaticMeshComponent *ball, USceneComponent *scene, USpringArmComponent *springarm, UCameraComponent* camera);
+
+	/* Put Components Settings */
+	void Setting();
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	void Azimuth(float Val);
+	void Elevation(float Val);
+
+	/** Called for side to side input */
+	void MoveRight(float Val);
+
+	/** Called to move ball forwards and backwards */
+	void MoveForward(float Val);
 
 public:	
 	// Called every frame
